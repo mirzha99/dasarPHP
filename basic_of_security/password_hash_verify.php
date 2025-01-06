4ac5d994d293e146935ce3ca3731052a4ca6250e
mirzha99 <muhammad.mirza3107@gmail.com>
2025-01-06 16:17:11 +0700

    password hash dan verifi

---
+++ basic_of_security/password_hash_verify.php
@@ -0,0 +1,59 @@
+<?php 
+function menu(){
+    echo "
+    +======================+
+    |1. set password       |
+    |2. get password hash  |
+    |3. verify password    |
+    |4. exit CLI           |
+    +======================+
+    ";
+}
+$password_hash = "";
+function set_password(){
+    echo "Set Password :";
+    $password = trim(fgets(STDIN));
+    global $password_hash;
+    $password_hash = password_hash($password,PASSWORD_DEFAULT);
+    echo "Berhasil Set Password".PHP_EOL;
+}
+function get_password_hash($password_hash){
+    if(empty($password_hash)){
+        echo "Password Belum di set".PHP_EOL;
+    }else{
+        echo "Password Hash : {$password_hash}".PHP_EOL;
+    }
+}
+function verify_password($password_hash){
+    echo "Masukan Password Anda :";
+    $password = trim(fgets(STDIN));
+    if(password_verify($password,$password_hash)){
+        echo "Password Benar".PHP_EOL;
+    }else{
+        echo "Password Salah".PHP_EOL;
+    }
+}
+
+while(true){
+    menu();
+    echo "Menu :";
+    $menu = trim(fgets(STDIN));
+    switch($menu){
+        case "1":
+            set_password();
+        break;
+        case "2":
+            get_password_hash($password_hash);
+        break;
+        case "3":
+            verify_password($password_hash);
+        break;
+        case "4":
+            exit();
+        break;
+        default:
+            echo "Menu {$menu} tidak ada !".PHP_EOL;
+        break;
+    }
+}
+;?>
